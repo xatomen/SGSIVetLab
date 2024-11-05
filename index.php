067f5c29f4fc9a82c6f685f6e51d5eae6a9e8d15
Jorge Gallardo <jorgito899@gmail.com>
2024-11-05 09:43:12 -0300

    Se quitó del archivo la parte de PHP

--- index.php
+++ index.php
@@ -2,43 +2,43 @@
 
 // Conexión a la BD
 require_once './config/database.php';
-
+require_once './src/login.php';
 // Ejemplo de inicio de sesión
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     $username = $_POST['username'];
+//     $password = $_POST['password'];
 
-    // Consultar la base de datos
-    $stmt = $conn->prepare("SELECT * FROM Credenciales WHERE Usuario = :username");
-    $stmt->bindParam(':username', $username);
-    $stmt->execute();
-    $user = $stmt->fetch();
+//     // Consultar la base de datos
+//     $stmt = $conn->prepare("SELECT * FROM Credenciales WHERE Usuario = :username");
+//     $stmt->bindParam(':username', $username);
+//     $stmt->execute();
+//     $user = $stmt->fetch();
 
-    echo $user['Usuario'];
-    echo $user['Contrasenha'];
+//     echo $user['Usuario'];
+//     echo $user['Contrasenha'];
 
-    if ($user && $password==$user['Contrasenha']) {
-        // Inicio de sesión exitoso
-        session_start();
-        $_SESSION['ID'] = $user['ID'];
-        $_SESSION['Usuario'] = $user['Usuario'];
-        // header('Location: pagina_principal.php');
+//     if ($user && $password==$user['Contrasenha']) {
+//         // Inicio de sesión exitoso
+//         session_start();
+//         $_SESSION['ID'] = $user['ID'];
+//         $_SESSION['Usuario'] = $user['Usuario'];
+//         // header('Location: pagina_principal.php');
 
-        // Verificar el tipo de usuario y redirigir
-        if ($user['Tipo_Usuario'] == 'Administrador') {
-            header('Location: ./public/admin.php');
-        } elseif ($user['Tipo_Usuario'] == 'Usuario') {
-            header('Location: ./public/user.php');
-        } else {
-            echo "Tipo de usuario desconocido";
-        }
-        exit;
+//         // Verificar el tipo de usuario y redirigir
+//         if ($user['Tipo_Usuario'] == 'Administrador') {
+//             header('Location: ./public/admin.php');
+//         } elseif ($user['Tipo_Usuario'] == 'Usuario') {
+//             header('Location: ./public/user.php');
+//         } else {
+//             echo "Tipo de usuario desconocido";
+//         }
+//         exit;
 
-    } else {
-        // Inicio de sesión fallido
-        echo "Usuario o contraseña incorrectos";
-    }
-}
+//     } else {
+//         // Inicio de sesión fallido
+//         echo "Usuario o contraseña incorrectos";
+//     }
+// }
 ?>
 
 
