eaa89e5ace5ac4199c8833608ebcc0ffca8cbcb6
Jorge Gallardo <jorgito899@gmail.com>
2024-11-05 09:42:48 -0300

    Se indica los puntos a implementar

--- public/admin/mostrar_registros.php
+++ public/admin/mostrar_registros.php
@@ -1,9 +1,22 @@
 <?php
- require_once '../../src/header_admin.php'
+    require_once '../../src/header_admin.php';
+    
 ?>
 
+<!-- Registros de uso de insumo -->
 
+
+<!-- Registros de ingresos de insumo -->
+
+
+<!-- Registros de mantención de entidades de insumo -->
+
+
+<!-- Registros de mantención de entidades de usuarios -->
+
+
+<!-- Registros de mantención de entidades de proveedores -->
 
 <?php
- require_once '../../src/footer.php'
+    require_once '../../src/footer.php';
 ?>
