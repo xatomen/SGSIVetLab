6e8f9d2ae457a862eecf2f7238f8e0011d4cf5c8
Jorge Gallardo <jorgito899@gmail.com>
2024-11-23 23:25:26 -0300

    Se integra DataTables para mejorar la visualización del listado de insumos

--- public/admin/listado_insumos.php
+++ public/admin/listado_insumos.php
@@ -5,33 +5,43 @@
     $sentenciaSQL= $conn->prepare("SELECT * FROM insumo");
     $sentenciaSQL->execute();
     $listaInsumos=$sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
 
+<!-- Incluye las librerías de DataTables -->
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+<script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+
 <!-- Listado -->
-    <div class="card row m-5 shadow overflow-scroll">
-        <table class="table table-bordered">
-            <thead>
-                <h4 class="p-2">Listado de insumos</h4>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>ID</td>
-                    <td>Nombre insumo</td>
-                    <td>Cantidad</td>
-                    <td>Stock mínimo</td>
-                </tr>
-                <?php foreach($listaInsumos as $lista){?>
-                <tr>
-                    <td><?php echo $lista['ID'] ?></td>
-                    <td><?php echo $lista['Nombre'] ?></td>
-                    <td><?php echo $lista['Cantidad'] ?></td>
-                    <td><?php echo $lista['Stock_minimo'] ?></td>
-                </tr>
-                <?php }?>
-            </tbody>
-        </table>
-    </div>
+<div class="card row m-2 shadow">
+    <table id="insumosTable" class="table table-bordered">
+        <thead>
+            <h4 class="p-2">Listado de insumos</h4>
+            <tr>
+                <th>ID</t>
+                <th>Nombre insumo</th>
+                <th>Cantidad</th>
+                <th>Stock mínimo</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($listaInsumos as $lista){?>
+            <tr>
+                <td><?php echo $lista['ID'] ?></td>
+                <td><?php echo $lista['Nombre'] ?></td>
+                <td><?php echo $lista['Cantidad'] ?></td>
+                <td><?php echo $lista['Stock_minimo'] ?></td>
+            </tr>
+            <?php }?>
+        </tbody>
+    </table>
+</div>
+
+<script>
+    $(document).ready( function () {
+        $('#insumosTable').DataTable();
+    });
+</script>
 
 <?php
     include_once("../../src/footer.php");
