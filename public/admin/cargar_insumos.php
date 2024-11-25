9c32f29fb0a4f6055d3aac4dbef4eb3f7f59856b
Jorge Gallardo <jorgito899@gmail.com>
2024-11-25 01:07:44 -0300

    Se mejora el diseño de la tabla de insumos al eliminar el borde y agregar un espaciado adicional.

--- public/admin/cargar_insumos.php
+++ public/admin/cargar_insumos.php
@@ -133,9 +133,10 @@
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
 
 <!-- Listado -->
-<div class="card row m-5 shadow">
-    <table id="insumosTable" class="table table-bordered">
+<div class="card row m-5 p-2 shadow">
+    <table id="insumosTable" class="table">
         <h4 class="p-2">Listado de insumos</h4>
+        <hr>
         <thead>
             <tr>
                 <th>ID</th>
