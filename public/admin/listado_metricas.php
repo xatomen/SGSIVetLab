c87a32b9aa668c4a9a5aa9e1988754f54806e480
Jorge Gallardo <jorgito899@gmail.com>
2024-11-25 22:48:37 -0300

    Hay que actualizar las querys para usar PDO

--- public/admin/listado_metricas.php
+++ public/admin/listado_metricas.php
@@ -2,38 +2,38 @@
 require_once '../../src/header_admin.php';
 include_once("../../config/database.php");
 
-// Conectar a la base de datos
-$conn = new mysqli($servername, $username, $password, $dbname);
+// // Conectar a la base de datos
+// $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
+// // Verificar la conexión
+// if ($conn->connect_error) {
+//     die("Conexión fallida: " . $conn->connect_error);
+// }
 
-$fechaInicio = isset($_POST['fechaInicio']) ? $_POST['fechaInicio'] : '';
-$fechaFin = isset($_POST['fechaFin']) ? $_POST['fechaFin'] : '';
+// $fechaInicio = isset($_POST['fechaInicio']) ? $_POST['fechaInicio'] : '';
+// $fechaFin = isset($_POST['fechaFin']) ? $_POST['fechaFin'] : '';
 
-// Obtener datos de métricas (cantidad de insumos utilizados por área)
-$sql = "SELECT area.Area, SUM(insumo_usado_empleado.Cantidad) as TotalInsumosUsados 
-        FROM insumo_usado_empleado 
-        INNER JOIN empleado ON insumo_usado_empleado.ID_Empleado = empleado.ID
-        INNER JOIN area ON empleado.ID_Area = area.ID 
-        WHERE insumo_usado_empleado.Fecha BETWEEN '$fechaInicio' AND '$fechaFin'
-        GROUP BY area.Area";
-$result = $conn->query($sql);
+// // Obtener datos de métricas (cantidad de insumos utilizados por área)
+// $sql = "SELECT area.Area, SUM(insumo_usado_empleado.Cantidad) as TotalInsumosUsados 
+//         FROM insumo_usado_empleado 
+//         INNER JOIN empleado ON insumo_usado_empleado.ID_Empleado = empleado.ID
+//         INNER JOIN area ON empleado.ID_Area = area.ID 
+//         WHERE insumo_usado_empleado.Fecha BETWEEN '$fechaInicio' AND '$fechaFin'
+//         GROUP BY area.Area";
+// $result = $conn->query($sql);
 
-$areas = [];
-$totales = [];
+// $areas = [];
+// $totales = [];
 
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $areas[] = $row['Area'];
-        $totales[] = $row['TotalInsumosUsados'];
-    }
-} else {
-    echo "0 resultados";
-}
-$conn->close();
+// if ($result->num_rows > 0) {
+//     while($row = $result->fetch_assoc()) {
+//         $areas[] = $row['Area'];
+//         $totales[] = $row['TotalInsumosUsados'];
+//     }
+// } else {
+//     echo "0 resultados";
+// }
+// $conn->close();
 ?>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
