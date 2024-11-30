cbd9c71a60f0c41505ceddeb6434bef387960f9f
Jorge Gallardo <jorgito899@gmail.com>
2024-11-29 22:47:08 -0300

    Se añade la visualización del total de insumos utilizados en user.php y se mejoran los enlaces de navegación.

--- public/user.php
+++ public/user.php
@@ -1,8 +1,58 @@
 <?php
- require_once '../src/header_user.php'
+    require_once '../config/database.php';
+    require_once '../src/header_user.php';
+
+    $sentenciaSQL = $conn->prepare("SELECT SUM(Cantidad) AS total_insumos FROM insumo_usado_empleado WHERE ID_Empleado = :ID_Empleado");
+    $sentenciaSQL->bindParam(":ID_Empleado", $_SESSION['ID']);
+    $sentenciaSQL->execute();
+    $insumos = $sentenciaSQL->fetch(PDO::FETCH_ASSOC);
+    $total_insumos = $insumos['total_insumos'];
+
 ?>
 
+<div class="row justify-content-center p-2">
 
+    <h2 class="titulo">Bienvenido</h2>
+    <hr>
+
+    <div class="row m-2">
+        <div class="col"></div>
+
+        <div class="col card-custom card-personas">
+            <i class="fas fa-box"></i>
+            <div class="content">
+                <div class="number"><?php echo $total_insumos; ?></div>
+                <div class="text">Insumos Utilizados</div>
+            </div>
+        </div> 
+
+        <div class="col"></div>
+    </div>
+
+    <hr>
+
+    
+    <div class="row m-2">
+        <div class="col-xl"></div>
+        <div class="col-auto">
+            <a href="http://localhost/SGSIVetLab/public/user/utilizar_stock.php" class="btn m-1 btn-fixed-size <?php if($url=="http://localhost/SGSIVetLab/public/admin/listado_metricas.php"){echo "active";}?>">
+                <i class="fas fa-box"></i><br>Utilizar stock
+            </a>
+        </div>
+        <div class="col-auto">
+            <a href="http://localhost/SGSIVetLab/public/user/perfiles_muestra.php" class="btn m-1 btn-fixed-size <?php if($url=="http://localhost/SGSIVetLab/public/admin/listado_insumos.php"){echo "active";}?>">
+                <i class="fas fa-vial"></i><br>Perfiles de muestra
+            </a>
+        </div>
+        <div class="col-auto">
+            <a href="http://localhost/SGSIVetLab/public/user/movimientos_realizados.php" class="btn m-1 btn-fixed-size <?php if($url=="http://localhost/SGSIVetLab/public/admin/cargar_insumos.php"){echo "active";}?>">
+                <i class="fas fa-exchange-alt"></i><br>Movimientos realizados
+            </a>
+        </div>
+        <div class="col-xl"></div>
+    </div>
+
+</div>
 
 <?php
  require_once '../src/footer.php'
